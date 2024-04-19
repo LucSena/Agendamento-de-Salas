4bf0f258862b133b5667c6c86a61de0091adbed2
Lucas Sena <79860759+LucSena@users.noreply.github.com>
2024-04-19 16:41:24 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="0; url=agendamento/paginicial.php">
+    <title>Redirecionando...</title>
+</head>
+
+<body>
+    <p>Se você não for redirecionado automaticamente, <a href="paginicial.php">clique aqui</a>.</p>
+</body>
+
+</html>
